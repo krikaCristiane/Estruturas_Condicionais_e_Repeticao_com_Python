b927bcb29c8efc623f025f2032ce3b92675bb82c
krikaCristiane <krika.folha@gmail.com>
2023-10-31 14:40:30 -0300

    first commit

---
+++ identacao_e_blocos.py
@@ -0,0 +1,12 @@
+def sacar(valor):
+    saldo = 500
+    
+    if saldo >=valor:
+        print("valor sacado")
+        print("retire seu dinheiro na boca do caixa.")
+
+print("Obrigada por ser  nosso cliente, tenho um bom dia")
+
+sacar(100)
+
+       
